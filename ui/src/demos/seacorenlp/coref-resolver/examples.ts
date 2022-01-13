bf3f8d099e250ab083dc0dcc856c89c4e71953bf
Wei Qi <wqleong24@gmail.com>
2022-01-13 12:26:42 +0800

    Edit Indonesian example text for Coref Demo

--- ui/src/demos/seacorenlp/coref-resolver/examples.ts
+++ ui/src/demos/seacorenlp/coref-resolver/examples.ts
@@ -1,9 +1,9 @@
 const examples: Record<string, string>[] = [
   {
     text:
-      "Presiden Joko Widodo dan Perdana Menteri Singapura Lee Hsien Loong akan bertemu di Bintan. Mereka akan bertemu untuk meresmikan pembukaan pariwisata perbatasan antara Indonesia dengan Singapura.",
+      "Presiden Joko Widodo dan Perdana Menteri Singapura Lee Hsien Loong akan bertemu di Bintan. Mereka mau meresmikan pembukaan pariwisata perbatasan. Jokowi sudah siap sambut PM Lee.",
     display:
-      "Presiden Joko Widodo dan Perdana Menteri Singapura Lee Hsien Loong akan bertemu di Bintan. Mereka akan bertemu untuk meresmikan pembukaan pariwisata perbatasan antara Indonesia dengan Singapura.",
+      "Presiden Joko Widodo dan Perdana Menteri Singapura Lee Hsien Loong akan bertemu di Bintan. Mereka mau meresmikan pembukaan pariwisata perbatasan. Jokowi sudah siap sambut PM Lee.",
   },
 ];
 
