8421438cf53a29d6f9efafc39ae9de50a7931faa
Raymond Ng <raymond.bcng@gmail.com>
2022-01-17 20:18:42 +0800

    [#40] add additonal examples for aspect based sentiment analysis demo

--- ui/src/demos/sg-nlp/aspect-based-sentiment-analysis/examples.ts
+++ ui/src/demos/sg-nlp/aspect-based-sentiment-analysis/examples.ts
@@ -1,8 +1,18 @@
 const examples: Record<string, any>[] = [
     {
-      aspects: ["Soup"],
-      sentence: "Soup is tasty but soup is a little salty.",
-      display: "Soup is tasty but soup is a little salty."
+      aspects: ["service", "decor"],
+      sentence: "Everything is always cooked to perfection , the service is excellent, the decor cool and understated.",
+      display: "everything is always cooked to perfection ..."
+    },
+    {
+      aspects: ["food", "portions"],
+      sentence: "The food was lousy - too sweet or too salty and the portions tiny.",
+      display: "The food was lousy - too sweet or too salty ..."
+    },
+    {
+      aspects: ["service"],
+      sentence: "To sum it up : service varies from good to mediorce , depending on which waiter you get ; generally it is just average ok .",
+      display: "To sum it up : service varies from good to mediorce ..."
     },
   ];
   
