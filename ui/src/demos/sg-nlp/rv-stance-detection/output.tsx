397284575b0412372874105faeed950350cb2f39
Jonathan Heng <jonheng@users.noreply.github.com>
2022-02-14 15:08:11 +0800

    [#44] Add output for rumor verification and stance detection demo

--- ui/src/demos/sg-nlp/rv-stance-detection/output.tsx
+++ ui/src/demos/sg-nlp/rv-stance-detection/output.tsx
@@ -1,11 +1,53 @@
+import { Table } from "antd";
 import * as React from "react";
 
-const Output = ({ responseData }: any) => {
+import { BsArrowReturnRight } from "react-icons/bs";
+
+interface OutputProps {
+  responseData: {
+    text: string[];
+    rumor_label: string;
+    stance_label: string[];
+  };
+}
+
+const Indent: React.FC = ({ children }) => {
   return (
     <div>
-      <div>
-        The model thinks that the original tweet is {responseData.predicted_y}{" "}
-        with a probability of {responseData.predicted_prob}%.
+      <BsArrowReturnRight style={{ marginRight: "10px" }} />
+      {children}
+    </div>
+  );
+};
+
+const Output = ({ responseData }: OutputProps) => {
+  const dataSource = responseData.text.map((t, i) => {
+    return {
+      key: i,
+      text: i > 0 ? <Indent>{t}</Indent> : t,
+      stance: i > 0 ? responseData.stance_label[i - 1] : "",
+    };
+  });
+  const columns = [
+    {
+      title: <b>Text</b>,
+      dataIndex: "text",
+      key: "text",
+    },
+    {
+      title: <b>Stance</b>,
+      dataIndex: "stance",
+      key: "stance",
+    },
+  ];
+
+  return (
+    <div>
+      <Table dataSource={dataSource} columns={columns} pagination={false} />
+
+      <div style={{ paddingTop: "12px" }}>
+        The model thinks that this conversation thread is a{" "}
+        <b>{responseData.rumor_label}</b>.
       </div>
     </div>
   );
