5ac9bacdebce7b9ed04cb00cd7ff689ba0d1da41
Jonathan Heng <jonheng@users.noreply.github.com>
2022-08-30 11:16:08 +0800

    [#7] Update demo examples

--- ui/src/demos/sg-nlp/coherence-modeling/examples.ts
+++ ui/src/demos/sg-nlp/coherence-modeling/examples.ts
@@ -12,9 +12,24 @@ const examples: Record<string, any>[] = [
       "Otherwise , actual profit is compared with the 300-day estimate . Source : Zacks Investment Research. " +
       "Companies listed below reported quarterly profit substantially different from the average of analysts ' estimates . " +
       "Estimated and actual results involving losses are omitted .",
-    display: "[WSJ Test Example]",
+    display:
+      "[WSJ Test Example 1] Companies listed below reported quarterly profit substantially different from...",
   },
-
+  {
+    text1:
+      "Japan had an unadjusted trade surplus of $ 1.82 billion for the first 10 days of October , down from $ 3.16 billion a year earlier ," +
+      " the Finance Ministry said . The latest drop shows the narrowing in the nation 's trade gap reflected in successive full monthly reports is continuing . " +
+      "The report follows five-consecutive declines in full monthly figures . Imports rose sharply in the period , " +
+      "to $ 5.19 billion from $ 4.04 billion a year earlier , a change of 28 % . Exports during the period were $ 7.01 billion , 2.6 % below $ 7.20 billion a year ago .",
+    text2:
+      "The latest drop shows the narrowing in the nation 's trade gap reflected in successive full monthly reports is continuing . " +
+      "Exports during the period were $ 7.01 billion , 2.6 % below $ 7.20 billion a year ago . " +
+      "The report follows five-consecutive declines in full monthly figures . Imports rose sharply in the period , " +
+      "to $ 5.19 billion from $ 4.04 billion a year earlier , a change of 28 % . Japan had an unadjusted trade surplus of " +
+      "$ 1.82 billion for the first 10 days of October , down from $ 3.16 billion a year earlier , the Finance Ministry said .",
+    display:
+      "[WSJ Test Example 2] Japan had an unadjusted trade surplus of $ 1.82 billion for the first 10 days of October...",
+  },
 ];
 
 export default examples;
