cf4c6e11f8f7238f56853af919feaa22d901c86a
Jonathan Heng <jonheng@users.noreply.github.com>
2022-08-31 22:06:26 +0800

    [#7] Update model config and output to be more descriptive.

--- ui/src/demos/sg-nlp/coherence-modeling/config.tsx
+++ ui/src/demos/sg-nlp/coherence-modeling/config.tsx
@@ -5,8 +5,16 @@ import { ModelConfig } from "../../models";
 const models: ModelConfig[] = [
   {
     id: "coherence-momentum",
-    desc:
-      "This is a neural network model that makes use of a momentum encoder and hard negative mining during training.",
+    desc: (
+      <>
+        This is a neural network model that makes use of a momentum encoder and
+        hard negative mining during training. This model is able to take in a
+        piece of text and output a coherence score. The coherence score is only
+        meant for comparison, i.e. it is only meaningful when used to compare
+        between two texts, and the text with the higher coherence score is
+        deemed to be more coherent by the model.
+      </>
+    ),
     displayName: "Coherence Momentum",
     usage: {
       fromBackend: true,
@@ -17,13 +25,17 @@ const models: ModelConfig[] = [
 const config: DemoConfig = {
   taskId: "coherence-modeling",
   title: "Coherence Modeling",
-  desc:
+  desc: (
     <div>
-      Coherence is a measure of how well a piece of text fits together. A coherent list of sentences should follow naturally from previous ones 
-      while an incoherent list of sentences might seem to be randomly put together. Traditional metrics such as BLEU and ROUGE are unable to give a
-      robust measure of coherence. Given the rapid advancements in neural methods, coherence models have become a viable alternative approach to 
+      Coherence is a measure of how well a piece of text fits together. A
+      coherent list of sentences should follow naturally from previous ones
+      while an incoherent list of sentences might seem to be randomly put
+      together. Traditional metrics such as BLEU and ROUGE are unable to give a
+      robust measure of coherence. Given the rapid advancements in neural
+      methods, coherence models have become a viable alternative approach to
       evaluating the coherence of a text.
-    </div>,
+    </div>
+  ),
   models: models,
 };
 
--- ui/src/demos/sg-nlp/coherence-modeling/output.tsx
+++ ui/src/demos/sg-nlp/coherence-modeling/output.tsx
@@ -3,9 +3,19 @@ import * as React from "react";
 const Output = ({ responseData }: any) => {
   return (
     <div>
-      {responseData["text1_score"] > responseData["text2_score"]
-        ? "Text 1 is rated to be more coherent than Text 2."
-        : "Text 2 is rated to be more coherent than Text 1."}
+      {responseData["text1_score"] > responseData["text2_score"] ? (
+        <>
+          Text 1 (score: <b>{responseData["text1_score"].toFixed(2)}</b>) is
+          rated to be more coherent than Text 2 (score:{" "}
+          <b>{responseData["text2_score"].toFixed(2)}</b>).
+        </>
+      ) : (
+        <>
+          Text 2 (score: <b>{responseData["text2_score"].toFixed(2)}</b>) is
+          rated to be more coherent than Text 1 (score:{" "}
+          <b>{responseData["text1_score"].toFixed(2)}</b>).
+        </>
+      )}
     </div>
   );
 };
