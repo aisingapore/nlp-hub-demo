5761197967ac4f6e829befc146b289bd7e3ce3d8
weiqipedia <56439386+weiqipedia@users.noreply.github.com>
2022-09-01 09:26:53 +0800

    Add example for coref demo (#8)

--- ui/src/demos/seacorenlp/coref-resolver/examples.ts
+++ ui/src/demos/seacorenlp/coref-resolver/examples.ts
@@ -5,6 +5,10 @@ const examples: Record<string, string>[] = [
     display:
       "Presiden Joko Widodo dan Perdana Menteri Singapura Lee Hsien Loong akan bertemu di Bintan. Mereka mau meresmikan pembukaan pariwisata perbatasan. Jokowi sudah siap sambut PM Lee.",
   },
+  {
+    text: "Presiden Jokowi menghadiri upacara kemerdekaan negara hari ini. Ia datang bersama dengan menteri-menterinya.",
+    display: "Presiden Jokowi menghadiri upacara kemerdekaan negara hari ini. Ia datang bersama dengan menteri-menterinya."
+  }
 ];
 
 export default examples;
