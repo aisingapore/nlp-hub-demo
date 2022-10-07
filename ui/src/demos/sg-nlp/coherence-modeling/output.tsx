4513c469f3f0eabe0fccbfa644668da8721fcfbe
Jonathan Heng <jonheng@users.noreply.github.com>
2022-10-07 10:58:15 +0800

    [#7] Add check for equal coherence scores

--- ui/src/demos/sg-nlp/coherence-modeling/output.tsx
+++ ui/src/demos/sg-nlp/coherence-modeling/output.tsx
@@ -3,19 +3,33 @@ import * as React from "react";
 const Output = ({ responseData }: any) => {
   return (
     <div>
-      {responseData["text1_score"] > responseData["text2_score"] ? (
-        <>
-          Text 1 (score: <b>{responseData["text1_score"].toFixed(2)}</b>) is
-          rated to be more coherent than Text 2 (score:{" "}
-          <b>{responseData["text2_score"].toFixed(2)}</b>).
-        </>
-      ) : (
-        <>
-          Text 2 (score: <b>{responseData["text2_score"].toFixed(2)}</b>) is
-          rated to be more coherent than Text 1 (score:{" "}
-          <b>{responseData["text1_score"].toFixed(2)}</b>).
-        </>
-      )}
+      {(() => {
+        if (responseData["text1_score"] === responseData["text2_score"]) {
+          return (
+            <>
+              Text 1 (score: <b>{responseData["text1_score"].toFixed(2)}</b>) is
+              rated to be equally coherent as Text 2 (score:{" "}
+              <b>{responseData["text2_score"].toFixed(2)}</b>).
+            </>
+          );
+        } else if (responseData["text1_score"] > responseData["text2_score"]) {
+          return (
+            <>
+              Text 1 (score: <b>{responseData["text1_score"].toFixed(2)}</b>) is
+              rated to be more coherent than Text 2 (score:{" "}
+              <b>{responseData["text2_score"].toFixed(2)}</b>).
+            </>
+          );
+        } else {
+          return (
+            <>
+              Text 2 (score: <b>{responseData["text2_score"].toFixed(2)}</b>) is
+              rated to be more coherent than Text 1 (score:{" "}
+              <b>{responseData["text1_score"].toFixed(2)}</b>).
+            </>
+          );
+        }
+      })()}
     </div>
   );
 };
